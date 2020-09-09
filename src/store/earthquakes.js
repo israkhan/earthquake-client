2c5d6ec765452b0fcbcdd1008dfe65cee3482e72
Isra Khan <israkhan2@gmail.com>
2020-09-09 12:04:00 -0600

    add earthquakes reducer to grab earthquakes based on search query

---
+++ src/store/earthquakes.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+/**
+ * ACTION TYPES
+ */
+const SET_EARTHQUAKE_SEARCH_RESULT = "SET_EARTHQUAKE_SEARCH_RESULT";
+
+/**
+ * INITIAL STATE
+ */
+const defaultState = [];
+
+/**
+ * ACTION CREATORS
+ */
+const setEarthquakeSearchResult = (earthquakes) => ({
+  type: SET_EARTHQUAKE_SEARCH_RESULT,
+  earthquakes,
+});
+
+/**
+ * THUNK CREATORS
+ */
+export const getSearchResult = (location, radius, start, end) => async (
+  dispatch
+) => {
+  try {
+    const response = axios(
+      `/api/earthquakes/search?location=${location}&raidus=${radius}&startDate=${start}&endDate=${end}`
+    );
+
+    dispatch(SET_EARTHQUAKE_SEARCH_RESULT(response.data));
+  } catch (err) {
+    console.error(err);
+    // dispatch(setQueryError(err.message));
+  }
+};
+
+/**
+ * REDUCER
+ */
+export default function (state = defaultState, action) {
+  switch (action.type) {
+    case SET_EARTHQUAKE_SEARCH_RESULT:
+      return action.earthquakes;
+    default:
+      return state;
+  }
+}
